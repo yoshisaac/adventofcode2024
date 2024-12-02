12f48ced53c093f54cee1756602cc8b488f2ce19
yoshisaac <isfortner@gmail.com>
2024-12-02 00:56:16 -0500

    Add day 2 part 1 stuff

---
+++ python/day2_1.py
@@ -0,0 +1,16 @@
+#!/bin/python
+f = open("input2_1", "r").read()
+
+lines = f.split("\n")
+lines.pop(len(lines)-1)
+
+num_of_safe:int = 0
+
+for line in lines: 
+    numbers = list(map(int, line.split()));
+    if all(numbers[i] < numbers[i + 1] if abs(numbers[i] - numbers[i + 1]) >= 1 and abs(numbers[i] - numbers[i + 1]) <= 3 else False for i in range(len(numbers) - 1)) or all(numbers[i] > numbers[i + 1] if abs(numbers[i] - numbers[i + 1]) >= 1 and abs(numbers[i] - numbers[i + 1]) <= 3 else False for i in range(len(numbers) - 1)):
+        num_of_safe+=1
+    else:
+        num_of_not+=1
+
+print(num_of_safe)
